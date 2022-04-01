c9d1f86b4f79c7ebe26f31386eb9b4fe9d51424e
Krzysztof Duda <halidu13@gmail.com>
2022-04-01 12:25:39 +0200

    Added authentication with Spotify API

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,92 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 import Sidebar from "./components/sidebar/Sidebar";
-import { atom } from "jotai";
+import { atom, useAtom } from "jotai";
 import Home from "./pages/home/Home";
 import Player from "./components/player/Player";
 import styles from "./app.module.scss";
+import { nanoid } from "nanoid";
+import { atomWithStorage } from "jotai/utils";
 
-const playingAtom = atom();
+const codeAtom = atom("");
+const accessTokenAtom = atom("");
 
 function App() {
+  const [code, setCode] = useAtom(codeAtom);
+  const [accessToken, setAccessToken] = useAtom(accessTokenAtom);
+  const [loginUrl, setLoginUrl] = useState("");
+  const AUTHORIZE = "https://accounts.spotify.com/authorize";
+  const redirect_uri = "http://localhost:3000/callback/";
+  const client_id = import.meta.env.VITE_CLIENT_ID;
+  const client_secret = import.meta.env.VITE_CLIENT_SECRET;
+
+  useEffect(() => {
+    requestAuthorization();
+  }, []);
+
+  useEffect(() => {
+    let access_token = window.location.hash.substring(
+      window.location.hash.indexOf("=") + 1,
+      window.location.hash.lastIndexOf("&token_")
+    );
+    setAccessToken(access_token);
+  }, [window.location]);
+
+  const getSecrets = () => {
+    var options = {
+      type: "POST",
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        Authorization:
+          "Basic " + (client_id + ":" + client_secret).toString("base64"),
+      },
+    };
+
+    fetch(options)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => console.log(e));
+  };
+
+  const requestAuthorization = () => {
+    const scope = "user-read-private user-read-email";
+    let url = "https://accounts.spotify.com/authorize";
+    url += "?response_type=token";
+    url += "&client_id=" + encodeURIComponent(client_id);
+    url += "&scope=" + encodeURIComponent(scope);
+    url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
+    url += "&state=" + encodeURIComponent(nanoid());
+    setLoginUrl(url);
+  };
+
+  useEffect(() => {
+    if (!accessToken) return;
+    const get = async () => {
+      const response = await fetch("https://api.spotify.com/v1/me", {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + accessToken,
+        },
+      });
+      const user = await response.json();
+      console.log(user);
+    };
+    get();
+  }, [accessToken]);
+
+  if (!accessToken) return <a href={loginUrl}>Login to spotify</a>;
+
   return (
     <main className={styles.layout}>
       <section className={styles.main}>
         <Sidebar />
+        <p>{accessToken}</p>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,45 +4,12 @@ import Card from "@/components/card/Card";
 import styles from "./home.module.scss";
 import { atomWithStorage } from "jotai/utils";
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-    "X-RapidAPI-Key": "ce6affad17mshdb2a7ee7a40f6efp18a980jsn42953df98c35",
-  },
-};
-
-const dataAtom = atomWithStorage();
-
-const fetchData = atom(
-  (get) => get(dataAtom),
-  async (_get, set) => {
-    const response = await fetch(
-      "https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0",
-      options
-    );
-    set(dataAtom, await response.json());
-  }
-);
-
 const Home = () => {
-  const [data, compute] = useAtom(fetchData);
-
-  useEffect(() => {
-    compute();
-  }, []);
-
-  if (!data) return <>Loading...</>;
-
   return (
     <main>
       <section className={styles.section__container}>
         <h4>Top Tracks</h4>
-        <div className={styles.section__list}>
-          {data.tracks.map((track) => (
-            <Card key={track.key} {...track} />
-          ))}
-        </div>
+        <div className={styles.section__list}></div>
       </section>
     </main>
   );
